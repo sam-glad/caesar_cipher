e9cab8307e6dbbd1f8babb3e599696ef03c9640a
Sam Gladstone <gladston@umich.edu>
2014-07-24 13:18:04 -0400

    Clean up, add tests

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,33 +1,38 @@
 require_relative '../message'
 
 describe Message do
-  it 'should remove all characters which are not letters' do
+  it 'should remove all non-space characters which are not letters' do
     @message = Message.new('@fo?o19')
-    @message.caesar(0).should == 'FOO'
+    @message.caesar_encrypt(0).should == 'FOO'
   end
 
   it 'should apply no shifts when passed 0' do
     @message = Message.new('foo')
-    @message.caesar(0).should == @message.upcase
+    @message.caesar_encrypt(0).should == @message.text.upcase
   end
 
   it 'should apply no changes to an upper-case message when passed 0' do
     @message = Message.new('FOO')
-    @message.caesar(0).should == @message
+    @message.caesar_encrypt(0).should == @message.text.upcase
   end
 
   it 'should apply the same change to all letters' do
     @message = Message.new('aaaaa')
-    @message.caesar(1).should == 'bbbbb'
+    @message.caesar_encrypt(1).should == 'BBBBB'
   end
 
   it 'should start at the beginning of the alphabet after passing \'z\'' do
     @message = Message.new('xyz')
-    @message.caesar(3).should == 'abc'
+    @message.caesar_encrypt(3).should == 'ABC'
   end
 
   it 'should keep spaces' do
     @message = Message.new(' this is a 9message9 ')
-    @message.caesar(5).should == ' YMNX NX F RJXXFLJ '
+    @message.caesar_encrypt(5).should == ' YMNX NX F RJXXFLJ '
+  end
+
+  it 'should work with negative numbers' do
+    @message = Message.new('abc')
+    @message.caesar_encrypt(-3).should == 'XYZ'
   end
 end
